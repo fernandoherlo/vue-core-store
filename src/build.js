42467ac0c902cb07cab866eacfc2ad1319713c9b
Fernando Hernáez <fernando.herlo@gmail.com>
2020-05-13 15:54:01 +0200

    Bugs

---
+++ src/build.js
@@ -0,0 +1,16 @@
+/**
+ * @name VueJS VueCore Store (vue-core-store)
+ * @description Library for Vue.js 2.0
+ */
+// Vuex
+import VuexLoad from '@/vuex/modules/load'
+import VuexGetters from '@/vuex/getters'
+import VuexActions from '@/vuex/actions'
+import VuexMutations from '@/vuex/mutations'
+
+export {
+  VuexLoad,
+  VuexGetters,
+  VuexActions,
+  VuexMutations
+}
